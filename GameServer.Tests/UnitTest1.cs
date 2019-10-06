f8ff9639c0fdd12b5f5352009bad4031725f6b46
j-harr <jacob-harrison@outlook.com>
2019-10-05 19:42:20 -0500

    Create source and test directories.

---
+++ GameServer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using GameServer.Models;
+
+namespace GameServer.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            GameBoard board = new GameBoard(1,1);
+            
+        }
+    }
+}
---
+++ GameServer/Models/GameBoard.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace GameServer.Models
+{
+    public class GameBoard
+    {
+       public GameBoard(int x, int y) {}
+    }
+}
