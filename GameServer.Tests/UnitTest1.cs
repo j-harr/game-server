e15ac00d3d66b584e2ce3db8b4934e573f8a0f20
j-harr <jacob-harrison@outlook.com>
2019-10-05 23:36:13 -0500

    Create GameBoard class.

--- GameServer.Tests/UnitTest1.cs
+++ GameServer.Tests/UnitTest1.cs
@@ -4,13 +4,71 @@ using GameServer.Models;
 
 namespace GameServer.Tests
 {
-    public class UnitTest1
+    public class GameBoardTest
     {
         [Fact]
-        public void Test1()
+        public void gameBoardUnitDimensions()
         {
-            GameBoard board = new GameBoard(1,1);
+            GameBoard<int> board = new GameBoard<int>(1,1);
+            Assert.Equal(1, board.getArea());
+            Assert.Equal(1, board.Width);
+            Assert.Equal(1, board.Height);
+        }
+
+        [Fact]
+        public void gameBoardNonTrivialDimensions()
+        {
+            GameBoard<int> board = new GameBoard<int>(2, 3);
+            Assert.Equal(6, board.getArea());
+            Assert.Equal(2, board.Width);
+            Assert.Equal(3, board.Height);
+        }
+
+        [Fact]
+        public void setItemInCell()
+        {
+            GameBoard<char> board = new GameBoard<char>(3, 4);
+            board.set(1, 1, 'o');
+            Assert.Equal('o', board.get(1, 1));
+            board.set(0, 0, 'x');
+            Assert.Equal('x', board.get(0, 0));
+        }
+
+        [Fact]
+        public void setItemInCellOutOfRange()
+        {
+            GameBoard<char> board = new GameBoard<char>(3, 4);
+            Assert.Throws<IndexOutOfRangeException>(() => board.set(-1, 0, 'f'));
+            Assert.Throws<IndexOutOfRangeException>(() => board.set(3, 1, 'l'));
+            Assert.Throws<IndexOutOfRangeException>(() => board.set(1, 4, 'l'));
+            Assert.Throws<IndexOutOfRangeException>(() => board.set(1, -1, 'l'));
+            board.set(2, 3, 'r');
+        }
+
+        [Fact]
+        public void getBoardToString()
+        {
+            GameBoard<char> board = new GameBoard<char>(2, 2);
+            Assert.Equal("[[\0,\0],[\0,\0]]", board.ToString());
+
+            GameBoard<int> boardInt = new GameBoard<int>(2, 2);
+            Assert.Equal("[[0,0],[0,0]]", boardInt.ToString());
+
+            boardInt.set(1, 1, 9);
+            boardInt.set(0, 0, 1);
+            boardInt.set(0, 1, 2);
+            boardInt.set(1, 0, 3);
+            Assert.Equal("[[1,3],[2,9]]", boardInt.ToString());
+
+            GameBoard<int> complexBoard = new GameBoard<int>(2, 5);
+            complexBoard.set(0, 0, 1);
+            complexBoard.set(0, 1, 2);
+            complexBoard.set(1, 0, 3);
+            complexBoard.set(1, 1, 4);
+            complexBoard.set(1, 4, 587);
             
+            Assert.Equal("[[1,3],[2,4],[0,0],[0,0],[0,587]]", complexBoard.ToString());
+
         }
     }
 }
--- GameServer/Models/GameBoard.cs
+++ GameServer/Models/GameBoard.cs
@@ -3,8 +3,57 @@ using System.Collections.Generic;
 
 namespace GameServer.Models
 {
-    public class GameBoard
+    public class GameBoard<T>
     {
-       public GameBoard(int x, int y) {}
+        private T[] data;
+
+        private int width;
+
+        private int height;
+
+        public GameBoard(int width, int height) 
+        {
+            this.Width = width;
+            this.Height = height;
+            this.data = new T[width * height];
+        }
+
+        public int Height { get => height; set => height = value; }
+        public int Width { get => width; set => width = value; }
+
+        public int getArea() { return Width * Height; }
+
+        public void set(int w, int h, T value)
+        {
+            if(w >= this.Width || w < 0 || h >= this.Height || h < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            int access = h * this.Width + w;
+            this.data[access] = value;
+        }
+
+        public T get(int w, int h)
+        {
+            return this.data[h * this.Width + w];
+        }
+
+        public override string ToString()
+        {
+            string result = "[";
+            for(int h = 0; h < this.Height; h++)
+            {
+                result += "[";
+                for(int w = 0; w < this.Width; w++)
+                {
+                    result += (get(w, h) == null ? "" : get(w,h).ToString());
+                    if (w != this.Width - 1) result += ",";
+                }
+                result += "]";
+                if (h != this.Height - 1) result += ",";
+            }
+
+            return result += "]";
+        }
     }
 }
